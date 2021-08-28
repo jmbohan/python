70b376108e9d4fb3b7dd935dfd14696c17597393
jmbohan <jmbohan@purdue.edu>
2021-08-28 16:40:08 -0400

    part 1

---
+++ Ex2_1.py
@@ -0,0 +1,10 @@
+# Ex2_1
+
+num1 = 15
+num2 = 31.5
+
+print(num1 // 5)  # 15 divided by 5 is 3, I was correct
+print(num1 / 5.0)  # 15 divided by 5.0 is 3, I was incorrect the answer was 3.0
+print(num2 / 2.0)  # 31.5 divided by 2.0 is 15.75, I was correct
+print(num1 % 3)  # The remainder is 0, I was correct
+print(1 + num1 * 6)  # The answer should be 91, I was correct
