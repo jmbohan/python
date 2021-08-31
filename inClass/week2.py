17a3ccc6002cca9ba4bf9452ae43d389b50eaa42
jmbohan <jmbohan@purdue.edu>
2021-08-31 17:53:01 -0400

    ex.1

--- inClass/week2.py
+++ inClass/week2.py
@@ -1,1 +1,11 @@
 # week 2
+_wage_ = 0;
+
+while _wage_ <= 7.50 or not float:
+    _wage_= float(input("What is the hourly wage? "))
+    print("wage is not valid")
+    continue
+
+else:
+    _pay_ = str(_wage_ * 40)
+    print("You should pay your employee " + _pay_+ " dollars")
