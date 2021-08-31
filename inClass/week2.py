396308529b629ec9b5f667b782c08197c1552cb4
jmbohan <jmbohan@purdue.edu>
2021-08-31 18:28:00 -0400

    prof's code

--- inClass/week2.py
+++ inClass/week2.py
@@ -1,3 +1,4 @@
+'''
 # week 2
 _wage_ = 0;
 
@@ -9,3 +10,19 @@ while _wage_ < 7.50 or not float:
 else:
     _pay_ = str(_wage_ * 40)
     print("You should pay your employee " + _pay_+ " dollars")
+'''
+''' my code above was off by not handling the string error
+    this is the professors code
+'''
+while True:
+    try:
+        wage = input("Please insert your hourly wage: \n")
+        if float(wage) < 7.5:
+            print ("Not Valid \n")
+            continue
+        else:
+            salary = float(wage) * 40
+            print("You should pay your workers $", salary, "dollars")
+            break
+    except:
+        continue
