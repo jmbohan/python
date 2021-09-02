350142c7f4617f07f3917c9efca832190e9f642d
jmbohan <jmbohan@purdue.edu>
2021-09-02 15:34:43 -0400

    not a number added

--- inClass/week3.py
+++ inClass/week3.py
@@ -3,12 +3,16 @@ flag = True
 while flag == True:
     try:
         usr_num = input("Please enter a six digit number: ")
-        if len(usr_num) > 6 :
-                print("Number is too big!")
-        elif len(usr_num) < 6:
-                print("Number is too small!")
+        if float(usr_num):
+            if len(usr_num) > 6 :
+                    print("Number is too big!")
+            elif len(usr_num) < 6:
+                    print("Number is too small!")
+            else:
+                print(usr_num)
+                flag = False
         else:
-            print(usr_num)
-            flag = False
+            print("you did not enter a number")
     except:
+        print("you did not enter a number")
         pass
