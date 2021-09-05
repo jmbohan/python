d782e76f707f976856ecc17ad793048ed1de594c
jmbohan <jmbohan@purdue.edu>
2021-09-05 13:19:47 -0400

    updated chode

--- assignTwo/Ex5_1.py
+++ assignTwo/Ex5_1.py
@@ -2,15 +2,14 @@
 usr_num = 0
 count = 0
 while True:
-    try:
-        usr_num = usr_num + int(input("Please enter the number: "))
-        count = count + 1
-        # if not int entered 
-    except ValueError as ve: 
-        # check for done
-        # print(ve)
-        if str(ve).split(": ")[1].replace("'","") == "done": 
+        str_num = input("Enter the number: ")
+        if str_num == 'done':
             break
-        else:
-            print("You need to enter a integer")
+        try: 
+            int_val = int(str_num)
+        except:
+            print('Invalid input')
+            continue
+        count = count + 1
+        usr_num = usr_num + int_val
 print("Total: " + str(usr_num) + "\nCount: " + str(count) + "\nAverage: " + str(usr_num/count)) 
