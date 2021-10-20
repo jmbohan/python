c82a5d452e833be226aa216b1111b47e2d569bdf
jmbohan <jmbohan@purdue.edu>
2021-10-20 12:31:37 -0400

    update

--- project1/project1.py
+++ project1/project1.py
@@ -1,59 +1,30 @@
 import csv
 
-fhand = input('Enter file name: ')
-pilot_names = []
-data = []
-with open(fhand, 'r') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    for row in reader:
-        data.append(row)
-avg_first = 0
-avg_last = 0
-row_len = 0
-for row in reader:
-    avg_first += len(row[1])
-    avg_last += len(row[2])
-    row_len += 1
-avg_test1 = 0
-avg_test2 = 0
-avg_test3 = 0
-print(reader)
-# for row in reader:
-#     avg_test1 += int(row[6])
-#     avg_test2 += int(row[7])
-#     avg_test3 += int(row[8])
-# avg_test1 = int(avg_test1 / row_len)
-# avg_test2 = int(avg_test2 / row_len)
-# avg_test3 = int(avg_test3 / row_len)
-# colors = dict()
-# for row in reader:
-#     if row[5] in colors:
-#         colors[row[5]] += 1
-#     else:
-#         colors[row[5]] = 1
-# pilot_id = 0
-# highest_test = 0
-# best_pilot = dict()
-# for row in reader:
-#     test_sum = int(row[6]) + int(row[7]) + int(row[8])
-#     if test_sum > highest_test:
-#         highest_test = test_sum
-#         pilot_id = int(row[0]) - 1
-#         best_pilot['First Name'] = reader[pilot_id[1]]
-#         best_pilot['Last Name'] = reader[pilot_id[2]]
-#         best_pilot['Robot Serial'] = reader[pilot_id[4]]
-#         best_pilot['Color'] = reader[pilot_id[5]]
-#         best_pilot['Average Score'] = str(int(highest_test / 3))
-# print("Average first name length: %d" % avg_first)
-# print("Average Last name length: %d" % avg_last)
-# print("Average field test 1 Score: %d" % avg_first)
-# print("Average field test 2 Score: %d" % avg_test2)
-# print("Average field test 3 Score: %d" % avg_test3)
-#
-# print(best_pilot["FirstName"] + " " + best_pilot["LastName"] +
-#       ",Robot Serial" + best_pilot["Robot Serial"] + "," + best_pilot["Color"] +
-#       ", with an average field Test Score of " + best_pilot["AvgScore"])
-#
-# for color in colors:
-#     print(color + " : " + str(colors[color]) + ",")
-reader.close
+
+def open_file(fname):
+    # read the csv file
+    with open(fname, 'r') as csv_file:
+        # create a csv reader
+        csv_reader = csv.reader(csv_file)
+        # extract field names 
+        field_names = next(csv_reader)
+        # get data row by row 
+        for row in csv_reader:
+            rows.append(row)
+        return field_names, rows
+
+
+def name_avg(first_avg=0, last_avg=0, row_len=0):
+    for row in rows:
+        first_avg += len(row[1])
+        last_avg += len(row[2])
+        row_len += 1
+    first_avg = int(first_avg / row_len)
+    last_avg = int(last_avg / row_len)
+    print('Average first name length: {0}\nAverage last name length: {1}\n'.format(first_avg, last_avg))
+
+
+rows = []
+open_file(input('Enter file name: '))
+print('Test Site Report: ')
+name_avg()
--- project1/projectOne.py
+++ project1/projectOne.py
@@ -1,11 +1,75 @@
 import csv
-from pprint import pformat
 
-f = open('Pilots1.csv', 'r')
-myreader = csv.reader(f)
-lt = []
-for row in myreader:
-    lt.append(row)
-format_data = pformat(lt, )
-print(format_data)
-f.close()
+filename = input("Enter a pilot data file name: ")
+data = []
+
+with open(filename, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    fieldNames = csv_reader.__next__()
+
+    for row in csv_reader:
+        data.append(row)
+
+# Get average first and last name length
+avgFirst = 0
+avgLast = 0
+rowLen = 0
+for row in data:
+    avgFirst += len(row[1])
+    avgLast += len(row[2])
+    rowLen += 1
+avgFirst = int(avgFirst / rowLen)
+avgLast = int(avgLast / rowLen)
+print(avgFirst, avgLast)
+# get the average test score
+avgFieldOne = 0
+avgFieldTwo = 0
+avgFieldThree = 0
+for row in data:
+    avgFieldOne += int(row[6])
+    avgFieldTwo += int(row[7])
+    avgFieldThree += int(row[8])
+avgFieldOne = int(avgFieldOne / rowLen)
+avgFieldTwo = int(avgFieldTwo / rowLen)
+avgFieldThree = int(avgFieldThree / rowLen)
+print(avgFieldOne, avgFieldTwo, avgFieldThree)
+
+# get the best pilot
+bestId = 0
+bestFieldTestSum = 0
+tempFieldTestSum = 0
+for row in data:
+    tempFieldTestSum = int(row[6]) + int(row[7]) + int(row[8])
+if tempFieldTestSum > bestFieldTestSum:
+    bestFieldTestSum = tempFieldTestSum
+bestId = int(row[0]) - 1
+bestPilot = dict()
+bestPilot["FirstName"] = data[bestId][1]
+bestPilot["LastName"] = data[bestId][2]
+bestPilot["serialRS"] = data[bestId][4]
+bestPilot["Color"] = data[bestId][5]
+bestPilot["AvgScore"] = str(int(bestFieldTestSum / 3))
+# printing the data to the Console Now
+# Printing average First name length and Last Name length
+print("Average first name length: %d" % avgFirst)
+print("Average Last name length: %d" % avgLast)
+# Printing the three Field Test Scores' average
+print("Average field test 1 Score: %d" % avgFieldOne)
+print("Average field test 2 Score: %d" % avgFieldTwo)
+print("Average field test 3 Score: %d" % avgFieldThree)
+# printing the best pilot data
+print(bestPilot["FirstName"] + " " + bestPilot["LastName"] + ",serial RS: " + bestPilot["serialRS"] + "," + bestPilot[
+    "Color"] + ", with an average field Test Score of " + bestPilot["AvgScore"])
+# Getting Color Histograms, color histogram is represented by a dict
+colorHistogram = dict()
+
+for row in data:
+    # row[5] represents the color
+    if row[5] in colorHistogram:
+        colorHistogram[row[5]] += 1
+    else:
+        colorHistogram[row[5]] = 1
+
+# printing the colors Histogram
+for color in colorHistogram:
+    print(color + " : " + str(colorHistogram[color]) + ",")
