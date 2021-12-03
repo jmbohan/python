c70ba2d73f33a4f0680cefb3e7a84511f0ba257d
jmbohan <jmbohan@purdue.edu>
2021-12-03 14:30:33 -0500

    homework seven

--- assignSeven/HW_7.py
+++ assignSeven/HW_7.py
@@ -1,4 +1,5 @@
-# I have chosen to use Neural Networks
+# I have chosen to use Neural Networks and Naïve Bayes. The features I choose were ones that I felt had a
+# relatively high chance of being chocolate.
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -6,11 +7,16 @@ from sklearn.metrics import classification_report, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn import datasets
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+
 
 
 candy = pd.read_csv('candy-data.csv')
 
-X = candy.drop(['chocolate', 'competitorname'], axis=1)
+X = candy.drop(['chocolate', 'competitorname', 'winpercent', 'fruity','hard', 'pricepercent'], axis=1)
 y = candy['chocolate']
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
@@ -22,12 +28,13 @@ StandardScaler(copy=True, with_mean=True, with_std=True)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-mlp = MLPClassifier(hidden_layer_sizes=10, max_iter=2000)
+mlp = MLPClassifier(hidden_layer_sizes=(16, 16), max_iter=2000)
 mlp.fit(X_train, y_train)
 predictions = mlp.predict(X_test)
 y_test_results = np.array(y_test)
 # candy_confMatrix = confusion_matrix(y_true=y_test, y_pred=predictions)
-ConfusionMatrixDisplay.from_predictions(y_test_results, predictions)
+disp = ConfusionMatrixDisplay.from_predictions(y_test_results, predictions,)
+disp.ax_.set_title('Accuracy Score: {}'.format (accuracy_score(y_test, predictions)))
 plt.show()
 
 print(classification_report(y_test, predictions))
@@ -36,4 +43,24 @@ print(len(mlp.coefs_))
 print(len(mlp.coefs_[0]))
 print(len(mlp.intercepts_[0]))
 
+ConfusionMatrixDisplay.from_predictions(y_test_results, predictions)
 
+
+# Naïve Bayes.
+
+nav_candy = pd.read_csv('candy-data.csv')
+
+X = nav_candy.drop(['chocolate', 'competitorname', 'winpercent', 'fruity','hard', 'pricepercent'], axis=1)
+y = nav_candy['chocolate']
+# Split the data into a training set and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+gnb = GaussianNB()
+mnb = MultinomialNB()
+y_pred_gnb = gnb.fit(X_train, y_train).predict(X_test)
+cnf_matrix_gnb = confusion_matrix(y_test, y_pred_gnb)
+y_pred_mnb = mnb.fit(X_train, y_train).predict(X_test)
+cnf_matrix_mnb = confusion_matrix(y_test, y_pred_mnb) 
+disp = ConfusionMatrixDisplay.from_predictions(y_test, y_pred_mnb, cmap='Blues' )
+disp.ax_.set_title('Accuracy Score: {}'.format (accuracy_score(y_test, y_pred_mnb)))
+plt.show()
+print(classification_report(y_test, y_pred_mnb))
