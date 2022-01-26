2bfb812fa9fbe9c5d102d753c52ef2e84558196c
jmbohan <jmbohan@purdue.edu>
2022-01-26 12:21:18 -0500

    update

--- mid/mid_1.py
+++ mid/mid_1.py
@@ -1,6 +1,6 @@
 #Question 1 midterm
 while True:
-    sum = 2 
+    sum = 2
     try:
         n = input("Please enter a number: \n")
         if n == 'done': break
@@ -13,3 +13,4 @@ while True:
         print("Enter an integer or 'done' : ")
         continue
 
+# I need this
